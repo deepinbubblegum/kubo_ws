e59cea852587331ed8d64b569133ffe0ad65e8c5
deepinbubblegum <5911110222@mutacth.com>
2022-02-26 11:28:33 +0700

    add steering control in central control

--- src/central_control_system/scripts/central_control_system.py
+++ src/central_control_system/scripts/central_control_system.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import rospy
-from sensor_custom_msgs.msg import SensorPLCStamped
+from sensor_custom_msgs.msg import SteeringControlStamped, SensorPLCStamped
 
 class central_control_system:
     def __init__(self):
@@ -13,21 +13,45 @@ class central_control_system:
         # Get ros params
         self.get_ros_params()
 
+        # initial_variable
+        self.initial_variable()
+
         # create topic
+        self.sub_steering_control = rospy.Publisher(
+            'steering_control',
+            SteeringControlStamped,
+            self.steering_control_callback,
+            queue_size=1
+        )
+
         self.pub_plc_control = rospy.Publisher(
             'plc_control', 
             SensorPLCStamped, 
             queue_size=1)
 
+    # init variable
+    def initial_variable(self):
+        self.steer_control = 0
+
+    # callback funtion
+    def steering_control_callback(self, steer_control_msg):
+        self.steer_control = steer_control_msg.steer.SteerCMD
+
+    # end callback funtion
+
+    # get ros params
     def get_ros_params(self):
         self.frequency = rospy.get_param(self.node_name + '/frequency', 50)
         self.frame_id = rospy.get_param(self.node_name + '/frame_id', 'central_control')
 
+    # update rate pub to plc node
     def update(self):
         msg = SensorPLCStamped()
         msg.header.stamp = rospy.Time.now()
         msg.header.frame_id = self.frame_id
+        msg.PLC.WheelAngleLR = self.steer_control
     
+    # fun main
     def run(self):
         rate = rospy.Rate(self.frequency)
         while not rospy.is_shutdown():
