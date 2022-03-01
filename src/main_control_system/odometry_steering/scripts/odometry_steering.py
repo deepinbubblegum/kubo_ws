720fd52b55d5a05f68e4a9dac60c9722f02da3e5
deepinbubblegum <5911110222@mutacth.com>
2022-03-01 14:06:34 +0700

    ackermann odom (เหลือคำนวณ)

--- src/main_control_system/odometry_steering/scripts/odometry_steering.py
+++ src/main_control_system/odometry_steering/scripts/odometry_steering.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import rospy
-from sensor_custom_msgs.msg import SensorEncoderStamped
+from sensor_custom_msgs.msg import SensorEncoderStamped, SensorEncoderDistanStamped
 from geometry_msgs.msg import Twist, Quaternion, TransformStamped
 from nav_msgs.msg import Odometry
 from tf2_ros import TransformBroadcaster
@@ -32,14 +32,23 @@ class Odometry_ackermann:
 
     def set_variable(self):
         self.steer_position = 0.0
+        # distanc ticker
+        self.ticker = 0
+        self.prev_ticker = 0
+        self.multi_ticker = 0
+
         self.current_time = rospy.Time.now()
         self.last_time = rospy.Time.now()
 
     def callback_front_encoder(self, enc_msg):
-        self.steer_position = enc_msg.sensor.position
+        self.steer_position = self._encoder_wheel_steering(enc_msg.sensor.position)
+
+    def callback_distanc_enc(self, enc_msg):
+        self.ticker = self.multi_counting(enc_msg.sensor.ticker)
+        self.update()
 
     def update(self):
-        self.virtual_steering = self._encoder_wheel_steering(self.steering_position)
+        pass
 
     def create_topic_node(self):
         self.sub_steering_enc = rospy.Subscriber(
@@ -47,6 +56,13 @@ class Odometry_ackermann:
             SensorEncoderStamped,
             self.callback_front_encoder,
             queue_size=1)
+
+        self.sub_distanc_enc = rospy.Subscriber(
+            'recv_enc_topic',
+            SensorEncoderDistanStamped,
+            self.callback_distanc_enc,
+            queue_size=1
+        )
 
     def _encoder_wheel_steering(self, rad_enc):
         if rad_enc > 0.0:
@@ -59,6 +75,15 @@ class Odometry_ackermann:
             front_angle_wheel_l = 0.0
             front_angle_wheel_r = 0.0
         return (front_angle_wheel_l + front_angle_wheel_r) / 2
+
+    def multi_counting(self, ticker):
+        if ticker < self.ticks_min and self.prev_ticker > self.ticks_max:
+            self.multi_ticker += 1
+        if ticker > self.ticks_max and self.prev_ticker < self.ticks_min:
+            self.multi_ticker -= 1
+        distanc_ticker = 1.0 * (ticker + self.multi_ticker * (self.ticks_max - self.ticks_min))
+        self.prev_ticker = ticker
+        return distanc_ticker
 
     def run(self):
         rospy.spin()
